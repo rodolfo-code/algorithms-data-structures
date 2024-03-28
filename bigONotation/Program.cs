ca3a8c3e286f7f0227fbe3dd5033cd62b63f560a
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-03-28 06:29:36 -0300

    o(n)

---
+++ bigONotation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, mundo!");
+
+        // ALGORITMO LINEAR - O(n)
+        On_linear(new string[] { "Marcos", "Pedro", "Joao", "Felipe", "Rodolfo" });
+    }
+
+    public static void On_linear(string[] names)
+    {
+        foreach(var name in names)
+        {
+            if (name == "Rodolfo")
+                Console.WriteLine(name);
+        }
+    }
+}
