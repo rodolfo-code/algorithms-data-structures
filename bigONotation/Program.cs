276c25cc59f4eb231c255951521d0c537812a7c5
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-03-29 22:02:31 -0300

    iniciando com algoritmos

--- bigONotation/Program.cs
+++ bigONotation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -8,24 +9,86 @@ class Program
 
         string[] names = new string[] { "Marcos", "Pedro", "Joao", "Felipe", "Rodolfo" };
 
-        // O(n) - ALGORITMO LINEAR
-        On_linear(names);
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+        int[] nums2 = { 8, 9, 10 };
+
 
         // O(1) - CONSTANTE
         O1_constant(names);
+
+        // O(n) - ALGORITMO LINEAR
+        On_linear(names);
+
+        // O(n) - O(6 + 2n) => O(n)
+        On(names);
+
+        //O(n^2)
+        On2(nums);
+
+        // O(n*m)
+        Onm(nums, nums2);
+
+    }
+
+    public static void O1_constant(string[] names)
+    {
+        Console.WriteLine(names[0]);
     }
 
     public static void On_linear(string[] names)
     {
-        foreach(var name in names)
+        foreach (var name in names)
         {
             if (name == "Rodolfo")
                 Console.WriteLine(name);
         }
     }
 
-    public static void O1_constant(string[] names)
+    public static void On(string[] students)
     {
-        Console.WriteLine(names[0]);
+        var firtStudent = students[0]; /*O(1)*/
+        int total = 0; /*O(1)*/
+        string[] newList = new string[students.Length]; /*O(1)*/
+
+        foreach (var student in students)
+        {
+            total += 1; /*O(n)*/
+            newList.Append(student.ToString()); /*O(n)*/
+        }
+
+        Console.WriteLine(newList[1]); /*O(1)*/
+        Console.WriteLine(total); /*O(1)*/
+    }
+
+    public static void On2(int[] list) /*O(n^2)*/
+    {
+        int total = 0;
+        foreach(var num1 in list) 
+        { 
+            foreach(var num2 in list)
+            {
+                Console.WriteLine($"{num1} -  {num2}");
+                total += 1;
+            }
+        }
+
+        Console.WriteLine(total);
+    }
+
+    public static void Onm(int[] nums1, int[] nums2)
+    {
+        int total = 0;
+
+        foreach (var num1 in nums1)
+        {
+            foreach (var num2 in nums2)
+            {
+                Console.WriteLine($"{num1} -  {num2}");
+                total += 1;
+            }
+        }
+
+        Console.WriteLine(total);
+
     }
 }
